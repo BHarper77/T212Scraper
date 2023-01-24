b52f7bcb0a16da03beebea7c12902ea4d6d10b6d
BHarper7 <bradyharper11@googlemail.com>
2023-01-24 20:04:28 +0000

    Removed unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import dotenv from "dotenv"
-import { join } from "path"
 import { handler } from "./lambda";
 
 (async () => {
-	dotenv.config({ path: join(__dirname, "..", "config.env") })
-
-	if (process.env.NODE_ENV === "local") {
-		await handler()
-	}
+	await handler()
 })();
