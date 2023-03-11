6abbc36271627337c275a75c72b7f5cbe360d3a4
BHarper7 <bradyharper11@googlemail.com>
2023-03-11 14:34:00 +0000

    Dir fix

--- deploy/lambda.sh
+++ deploy/lambda.sh
@@ -1,5 +1,5 @@
 mkdir layers
-cp node_modules/playwright-core .
+cp -r node_modules/playwright-core .
 zip -r chromium.zip playwright-core 
 zip -r node_modules.zip node_modules
 mv -t layers node_modules.zip chromium.zip
