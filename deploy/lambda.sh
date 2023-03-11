e99ea810ea92890b5cdfcd96d801249a648a1d07
BHarper7 <bradyharper11@googlemail.com>
2023-03-11 14:41:31 +0000

    Possible fix

--- deploy/lambda.sh
+++ deploy/lambda.sh
@@ -1,5 +1,8 @@
 mkdir layers
-cp -r node_modules/playwright-core .
-zip -r chromium.zip playwright-core 
-zip -r node_modules.zip node_modules
+mv node_modules/playwright-core .
+zip -r chromium.zip playwright-core -q
+zip -r node_modules.zip node_modules -q
 mv -t layers node_modules.zip chromium.zip
+
+# move playwright-core back into node_modules for serverless build
+mv playwright-core node_modules/
