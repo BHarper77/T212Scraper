5eed54c9b8ba98f4725d0fadcbde789594352d49
BHarper7 <bradyharper11@googlemail.com>
2023-06-22 18:54:26 +0100

    Use s3 for deployment package

--- deploy/main.tf
+++ deploy/main.tf
@@ -39,35 +39,26 @@ resource "aws_iam_role" "iam_for_lambda" {
 resource "aws_lambda_function" "T212Scraper" {
   role             = aws_iam_role.iam_for_lambda.arn
   description      = "A web scraper that scrapes data from Trading 212 and writes to Stock Events and Google Sheets"
-  filename         = "../.serverless/t212scraper.zip"
   function_name    = "T212Scraper"
   handler          = "../.serverless/src/lambda.handler"
   runtime          = "nodejs18.x"
   source_code_hash = filebase64sha256("../.serverless/t212scraper.zip")
   timeout          = 600
-  layers           = [aws_lambda_layer_version.chromium.arn, aws_lambda_layer_version.dependencies.arn]
+  s3_bucket        = aws_s3_bucket.t212scraper.arn
+  s3_key           = "deploymentPackage.zip"
 
   tags = {
     Name = "T212Scraper"
   }
 }
 
-resource "aws_lambda_layer_version" "dependencies" {
-  filename         = "../layers/node_modules.zip"
-  layer_name       = "dependencies"
-  source_code_hash = filebase64sha256("../layers/node_modules.zip")
-
-  compatible_runtimes = ["nodejs18.x"]
-}
-
-resource "aws_lambda_layer_version" "chromium" {
-  filename         = "../layers/chromium.zip"
-  layer_name       = "chromium"
-  source_code_hash = filebase64sha256("../layers/chromium.zip")
-
-  compatible_runtimes = ["nodejs18.x"]
-}
-
 resource "aws_s3_bucket" "t212scraper" {
   bucket = "t212scraper"
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.t212scraper.arn
+  key    = "deploymentPackage.zip"
+  source = "../.serverless/t212scraper.zip"
+  etag   = filemd5("../.serverless/t212scraper.zip")
 }
